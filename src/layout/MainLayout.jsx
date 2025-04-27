adcc53b7d68711900f66f3b087ef3ad1f4b6f5c8
Farrux <Farrux@gmail.com>
2025-04-27 22:39:27 +0500

    feat: Enhance Chart and SideBar components; add routing and improve layout styles

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "../components/sidebar/SideBar";
 import { Outlet } from "react-router-dom";
 import FooterBar from "../pages/footerSideBar/FooterBar";
 import style from "./MainLayout.module.scss";
 
 function MainLayout() {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
-    <div className="main_layout">
-      <SideBar />
-      <main>
+    <div className={`main_layout ${showSidebar ? "sidebar" : ""}`}>
+      <SideBar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+      <main className={`main ${showSidebar ? "sidebar" : ""}`}>
         <Outlet />
       </main>
       <FooterBar />
--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -2,7 +2,7 @@ import Chart from "../../components/Chart";
 import { useCollectionsData } from "../../hooks/useCollectionsData";
 import style from "./Overview.module.scss";
 
-function Overview() {
+function Overview({ showSidebar }) {
   const { data, isPending } = useCollectionsData();
 
   return (
@@ -30,38 +30,40 @@ function Overview() {
         </div>
       </div>
       {/* Pots */}
-      <div className={style.overview__pots}>
-        <div className={style.pots__head}>
-          <h3 className={style.pots__title}>Pots</h3>
-          <p className={style.pots__details}>See Details </p>
-        </div>
-        <div className={style.pots__savingWrapper}>
-          <div className={style.pots__totalSaving}>
-            <img src="../images/icon-pot.svg" alt="image of pots" />
-            <div className={style.pots__totalCard}>
-              <p className={style.pots__totalCard__text}>Total Saved</p>
-              <h3 className={style.pots__totalCard__title}> $850</h3>
+      <div className={style.overview__statics}>
+        <div className={style.overview__pots}>
+          <div className={style.pots__head}>
+            <h3 className={style.pots__title}>Pots</h3>
+            <p className={style.pots__details}>See Details </p>
+          </div>
+          <div className={style.pots__savingWrapper}>
+            <div className={style.pots__totalSaving}>
+              <img src="../images/icon-pot.svg" alt="image of pots" />
+              <div className={style.pots__totalCard}>
+                <p className={style.pots__totalCard__text}>Total Saved</p>
+                <h3 className={style.pots__totalCard__title}> $850</h3>
+              </div>
+            </div>
+            <div className={style.pots__cardWrapper}>
+              {data &&
+                data.pots.map((p) => {
+                  return (
+                    <div key={p.id} className={style.pots__card}>
+                      <div className={style.pots__color}></div>
+                      <div className={style.pots__content}>
+                        <p>{p.name}</p>
+                        <h4>{p.total}</h4>
+                      </div>
+                    </div>
+                  );
+                })}
             </div>
           </div>
-          <div className={style.pots__cardWrapper}>
-            {data &&
-              data.pots.map((p) => {
-                return (
-                  <div key={p.id} className={style.pots__card}>
-                    <div className={style.pots__color}></div>
-                    <div className={style.pots__content}>
-                      <p>{p.name}</p>
-                      <h4>{p.total}</h4>
-                    </div>
-                  </div>
-                );
-              })}
-          </div>
         </div>
-      </div>
-      {/* Pie chart */}
-      <div>
-        <Chart />
+        {/* Pie chart */}
+        <div>
+          <Chart />
+        </div>
       </div>
     </div>
   );
