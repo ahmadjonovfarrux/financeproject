ff9a1fae88396047c851d544de28411d70e602f9
Farrux <Farrux@gmail.com>
2025-04-28 09:52:39 +0500

    feat: Update Overview and Transaction components; add Chart integration and improve layout styles

--- src/pages/transactions/Transactions.jsx
+++ src/pages/transactions/Transactions.jsx
@@ -3,8 +3,39 @@ import style from "./Transaction.module.scss";
 
 function Transactions() {
   const { data } = useCollection("transactions");
-  console.log(data);
-  return <div>Transactions</div>;
+  return (
+    <div className="transactions">
+      <div className="transactions__option">
+        <input type="text" placeholder="Search transaction" />
+      </div>
+      <div className="transactions__List">
+        <p className="transactionsList__text">Recipient / Sender</p>
+        <p className="transactionsList__text">Category</p>
+        <p className="transactionsList__text">Transaction Date</p>
+        <p className="transactionsList__text">Amount</p>
+      </div>
+      <div>
+        {data &&
+          data.map((d) => {
+            console.log(d);
+            return (
+              <div key={d.id} className={style.transactions__miniContainer}>
+                <div>
+                  <img
+                    className={style.miniContainer__img}
+                    src={d.avatar}
+                    alt={d.title}
+                  />
+                  <h3 className={style.miniContainer__title}>{d.title}</h3>
+                </div>
+                <p className={style.miniContainer__category}>{d.category}</p>
+                <p className={style.miniContainer__amount}>{d.amount}</p>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default Transactions;
